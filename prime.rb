dfac9ec17d0e208b58db50d4a0a3cb22832264a8
David Anderson <matandersonlv@hotmail.com>
2018-11-16 05:29:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,9 @@
-def prime?(n)
-  for num in 2..(n - 1)
-   if (n % num) == 0
+def prime?(number)
+  if number >= 2
+    (2..number - 1).all? do |x|
+      number % x != 0
+    end
+  else
     return false
-   end
   end
-   true
- end
+end
